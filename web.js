7f5f710270a333c366808385afff8253e089b518
Assaf Kozlowsky <assafko@gmail.com>
2013-07-14 17:55:00 +0300

    Update web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   //buf.toString(, "utf-8"))
   //response.send('Hello World3!');
   
-  fs.readFile('.index.html', 'utf8', function(err, data) {
+  fs.readFile('index.html', 'utf8', function(err, data) {
   if (err) throw err;
   response.send(data);
 });
