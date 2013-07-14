af9ee70a4c57b2a0f387433e10767b6669388225
Assaf Kozlowsky <assafko@gmail.com>
2013-07-14 17:47:06 +0300

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  //var frosty = new Buffer(24);
+  //fs.readFileSync(index.html)
+  //buf.toString(, "utf-8"))
+  //response.send('Hello World3!');
+  
+  fs.readFile(idnex.html, 'utf8', function(err, data) {
+  if (err) throw err;
+  response.send(data);
+});
+
 });
 
 var port = process.env.PORT || 5000;
